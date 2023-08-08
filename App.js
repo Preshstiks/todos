4b9f3e55c108910c4aff9151604a2df5a207b34a
Davies Precious <preshy.d5@gmail.com>
2023-08-08 03:07:48 +0100

    changes made

--- App.js
+++ App.js
@@ -48,12 +48,18 @@ export default function App() {
         <View style={styles.content}>
           <AddTodo submitHandler={submitHandler} />
           <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) => (
-                <TodoItems presshandler={presshandler} item={item} />
-              )}
-            />
+            {todos.length >= 1 ? (
+              <FlatList
+                data={todos}
+                renderItem={({ item }) => (
+                  <TodoItems presshandler={presshandler} item={item} />
+                )}
+              />
+            ) : (
+              <View style={styles.noTodos}>
+                <Text style={styles.emptyText}>You have no todos!</Text>
+              </View>
+            )}
           </View>
         </View>
       </View>
@@ -73,5 +79,16 @@ const styles = StyleSheet.create({
   list: {
     marginTop: 20,
     flex: 1,
+  },
+  noTodos: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emptyText: {
+    fontSize: 22,
+    letterSpacing: 2,
+    fontWeight: "600",
+    color: "#7f7f7f",
   },
 });
